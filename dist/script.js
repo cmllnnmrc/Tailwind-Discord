ef5e8959e761f4cb0b615ec1e0fc56140c6a496c
camille <cmarca01@gmail.com>
2023-05-28 01:23:21 +0800

    First Commit

---
+++ dist/script.js
@@ -0,0 +1,46 @@
+// const faqs = document.querySelectorAll(".faq__ques h4");
+// faqs.forEach((drop) => {
+//   drop.addEventListener("click", () => {
+//     drop.nextElementSibling.classList.toggle("open");
+//     drop.querySelector("i").classList.toggle("open");
+//   });
+// });
+
+const toggleMenu = document.querySelector(".toggle__menu");
+const mobileNav = document.querySelector(".header__action");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  mobileNav.classList.toggle("open");
+});
+
+// const links = document.querySelectorAll(".footer__item h5");
+// links.forEach((down) => {
+//   down.addEventListener("click", () => {
+//     down.nextElementSibling.classList.toggle("open");
+//     down.querySelector("i").classList.toggle("open");
+//   });
+// });
+
+// const faqNav = document.querySelectorAll(".faq-nav-link");
+// const faqContent = document.querySelectorAll(".faq__tab__content");
+// faqNav.forEach((faq) => {
+//   faq.addEventListener("click", () => {
+//     removeActiveFaq();
+//     faq.classList.add("active");
+//     const activeFaqContent = document.querySelector(`#${faq.id}-content`);
+//     removeActiveFaqContent();
+//     activeFaqContent.classList.add("active");
+//   });
+// });
+
+// function removeActiveFaq() {
+//   faqNav.forEach((faq) => {
+//     faq.classList.remove("active");
+//   });
+// }
+
+// function removeActiveFaqContent() {
+//   faqContent.forEach((faq) => {
+//     faq.classList.remove("active");
+//   });
+// }
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#23272A',
+        light: '#ffffff',
+        primary: '#404EED',
+        grey: '#F6F6F6',
+      },
+
+      screens: {
+        'sm': '500px',
+        'md': '768px',
+        'lg': '1080px',
+        'xl': '1920'
+      },
+
+      backgroundImage: {
+        'hmBG': 'url("./dc-img/hm-bg.svg")',
+      },
+    },
+  },
+  plugins: [],
+}
+
