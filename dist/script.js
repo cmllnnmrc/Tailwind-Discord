aa5819b6daeaf8df64f575ce6bd373c179f2c985
cmllnnmrc <cuetolovely0118@gmail.com>
2023-05-29 15:49:23 +0800

    Home Complete

--- dist/script.js
+++ dist/script.js
@@ -1,16 +1,18 @@
+const toggleMenu = document.querySelector(".toggle__menu");
+const mobileNav = document.querySelector(".header__action");
+const headBlur = document.querySelector(".blur");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  mobileNav.classList.toggle("open");
+  headBlur.classList.toggle("open");
+});
+
 const faqs = document.querySelectorAll(".footer__item h4");
 faqs.forEach((drop) => {
   drop.addEventListener("click", () => {
     drop.nextElementSibling.classList.toggle("open");
     drop.querySelector("i").classList.toggle("open");
   });
-});
-
-const toggleMenu = document.querySelector(".toggle__menu");
-const mobileNav = document.querySelector(".header__action");
-toggleMenu.addEventListener("click", () => {
-  toggleMenu.classList.toggle("open");
-  mobileNav.classList.toggle("open");
 });
 
 // const links = document.querySelectorAll(".footer__item h5");
